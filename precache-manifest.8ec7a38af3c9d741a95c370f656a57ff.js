6b7f612b450b6388eff8b7a5c3ac4d3bf6875d29
LukasG85 <lukas85b@gmail.com>
2019-12-04 11:46:07 +0100

    Updates

---
+++ precache-manifest.8ec7a38af3c9d741a95c370f656a57ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d1539af73a6c715413606fa60e3967",
+    "url": "/advent/index.html"
+  },
+  {
+    "revision": "1436d0f2580a14ea273e",
+    "url": "/advent/static/css/main.996a2747.chunk.css"
+  },
+  {
+    "revision": "4ed05ec1dfa2081b4b4c",
+    "url": "/advent/static/js/2.172bd3e4.chunk.js"
+  },
+  {
+    "revision": "1436d0f2580a14ea273e",
+    "url": "/advent/static/js/main.5955ca43.chunk.js"
+  },
+  {
+    "revision": "bb820709edf8753095b3",
+    "url": "/advent/static/js/runtime-main.bff4915d.js"
+  },
+  {
+    "revision": "3e0663a79fdffa441a770ea703b59860",
+    "url": "/advent/static/media/bcg.3e0663a7.jpg"
+  },
+  {
+    "revision": "645880807ec8205fbbc94489f23277b5",
+    "url": "/advent/static/media/bcg2.64588080.jpg"
+  }
+]);
